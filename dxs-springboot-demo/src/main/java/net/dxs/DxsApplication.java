85ead58ace3a7d584b55bf4844c821fa458a30fa
深情小建 <374452668@qq.com>
2018-08-16 22:37:14 +0800

    初始提交

---
+++ dxs-springboot-demo/src/main/java/net/dxs/DxsApplication.java
@@ -0,0 +1,12 @@
+package net.dxs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DxsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DxsApplication.class, args);
+	}
+}
