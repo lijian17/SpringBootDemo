26700458398dc7075a091a7e189e039694667fde
深情小建 <374452668@qq.com>
2018-08-16 22:57:19 +0800

    添加格式化的Javabean

--- dxs-springboot-demo/src/main/java/net/dxs/pojo/User.java
+++ dxs-springboot-demo/src/main/java/net/dxs/pojo/User.java
@@ -2,12 +2,20 @@ package net.dxs.pojo;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 public class User {
 	private String name;
+	@JsonIgnore
 	private String password;
 	private Integer age;
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", locale = "zh", timezone = "GMT+8")
 	private Date birthday;
 
+	@JsonInclude(Include.NON_NULL)
 	private String desc;
 
 	public String getName() {
