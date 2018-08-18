75f5b8842390870ee6ab7130f97315651d2f778f
深情小建 <374452668@qq.com>
2018-08-18 18:14:36 +0800

    SpringBoot资源文件属性配置 1、pom.xml配置依赖 2、配置.properties属性文件 3、Javabean属性与配置文件属性建立名称映射 4、Javabean的使用

--- dxs-springboot-demo/src/main/java/net/dxs/controller/HelloController.java
+++ dxs-springboot-demo/src/main/java/net/dxs/controller/HelloController.java
@@ -1,14 +1,29 @@
 package net.dxs.controller;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import net.dxs.pojo.DxsJSONResult;
+import net.dxs.pojo.Resource;
 
 @RestController
 public class HelloController {
 	
+	@Autowired
+	private Resource resource;
+	
 	@RequestMapping("/hello")
 	public Object hello() {
 		return "hello springboot";
+	}
+	
+	@RequestMapping("/getResource")
+	public DxsJSONResult getResource() {
+		Resource bean = new Resource();
+		BeanUtils.copyProperties(resource, bean);
+		return DxsJSONResult.ok(bean);
 	}
 
 }
