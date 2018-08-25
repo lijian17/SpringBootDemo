e1716d1e4cba2c6f28ee1a1b526d749a22d84cbd
深情小建 <374452668@qq.com>
2018-08-25 16:38:59 +0800

    二、实现基于mybatis的CRUD功能 1、配置要扫描的包 @SpringBootApplication // 扫描mybatis mapper 包路径 @MapperScan(basePackages = "net.dxs.mapper") // 扫描 所有需要的包，包含一些自用的工具类包 所在的路径 @ComponentScan(basePackages = { "net.dxs", "org.n3r.idworker" }) 2、创建service 1）前面第一步通过使用generatorConfig生成mapper以及pojo，我们已经得到了想要的mapper和pojo 2）创建UserService接口并实现 2、业务类调用 1）示例用到了一个分布式id生成器，非常实用 2）注意要在application中配置扫描路径，以便加载该工具类 报错如下： org.mybatis.spring.MyBatisSystemException: nested exception is org.apache.ibatis.builder.BuilderException: Error invoking SqlProvider method (tk.mybatis.mapper.provider.base.BaseInsertProvider.dynamicSQL).  Cause: java.lang.InstantiationException: tk.mybatis.mapper.provider.base.BaseInsertProvider 报错原因：导包错误 错误：import org.mybatis.spring.annotation.MapperScan; 正确：import tk.mybatis.spring.annotation.MapperScan; 3）保存 4）更新 4）注意我们应该 使用userMapper.updateByPrimaryKeySelective(user); 而不要实用userMapper.updateByPrimaryKey(user); 除非有特别需求的时候才用userMapper.updateByPrimaryKey(user); 5）删除 6）查询 7）查询所有

--- dxs-springboot-demo/src/main/java/net/dxs/DxsApplication.java
+++ dxs-springboot-demo/src/main/java/net/dxs/DxsApplication.java
@@ -2,8 +2,15 @@ package net.dxs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+// 扫描mybatis mapper 包路径
+@MapperScan(basePackages = "net.dxs.mapper")
+// 扫描 所有需要的包，包含一些自用的工具类包 所在的路径
+@ComponentScan(basePackages = { "net.dxs", "org.n3r.idworker" })
 public class DxsApplication {
 
 	public static void main(String[] args) {
---
+++ dxs-springboot-demo/src/main/java/net/dxs/controller/MyBatisCRUDController.java
@@ -0,0 +1,123 @@
+package net.dxs.controller;
+
+import java.util.Date;
+import java.util.List;
+
+import org.n3r.idworker.Sid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.dxs.pojo.DxsJSONResult;
+import net.dxs.pojo.SysUser;
+import net.dxs.service.UserService;
+
+@RestController
+@RequestMapping("mybatis")
+public class MyBatisCRUDController {
+
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private Sid sid;
+
+	@RequestMapping("/saveUser")
+	public DxsJSONResult saveUser() throws Exception {
+
+		String userId = sid.nextShort();
+
+		SysUser user = new SysUser();
+		user.setId(userId);
+		user.setUsername("dxs" + new Date());
+		user.setNickname("dxs" + new Date());
+		user.setPassword("abc123");
+		user.setIsDelete(0);
+		user.setRegistTime(new Date());
+
+		userService.saveUser(user);
+
+		return DxsJSONResult.ok("保存成功");
+	}
+
+	@RequestMapping("/updateUser")
+	public DxsJSONResult updateUser() {
+
+		SysUser user = new SysUser();
+		user.setId("10011001");
+		user.setUsername("10011001-updated" + new Date());
+		user.setNickname("10011001-updated" + new Date());
+		user.setPassword("10011001-updated");
+		user.setIsDelete(0);
+		user.setRegistTime(new Date());
+
+		userService.updateUser(user);
+
+		return DxsJSONResult.ok("保存成功");
+	}
+
+	@RequestMapping("/deleteUser")
+	public DxsJSONResult deleteUser(String userId) {
+		userService.deleteUser(userId);
+		return DxsJSONResult.ok("删除成功");
+	}
+
+	@RequestMapping("/queryUserById")
+	public DxsJSONResult queryUserById(String userId) {
+		return DxsJSONResult.ok(userService.queryUserById(userId));
+	}
+
+	@RequestMapping("/queryUserList")
+	public DxsJSONResult queryUserList() {
+
+		SysUser user = new SysUser();
+//		user.setUsername("dxs");
+//		user.setNickname("lijian");
+		user.setPassword("abc123");
+
+		List<SysUser> userList = userService.queryUserList(user);
+
+		return DxsJSONResult.ok(userList);
+	}
+
+//	@RequestMapping("/queryUserListPaged")
+//	public DxsJSONResult queryUserListPaged(Integer page) {
+//
+//		if (page == null) {
+//			page = 1;
+//		}
+//
+//		int pageSize = 10;
+//
+//		SysUser user = new SysUser();
+////		user.setNickname("lijian");
+//
+//		List<SysUser> userList = userService.queryUserListPaged(user, page, pageSize);
+//
+//		return DxsJSONResult.ok(userList);
+//	}
+//
+//	@RequestMapping("/queryUserByIdCustom")
+//	public DxsJSONResult queryUserByIdCustom(String userId) {
+//
+//		return DxsJSONResult.ok(userService.queryUserByIdCustom(userId));
+//	}
+//
+//	@RequestMapping("/saveUserTransactional")
+//	public DxsJSONResult saveUserTransactional() {
+//
+//		String userId = sid.nextShort();
+//
+//		SysUser user = new SysUser();
+//		user.setId(userId);
+//		user.setUsername("lijian" + new Date());
+//		user.setNickname("lijian" + new Date());
+//		user.setPassword("abc123");
+//		user.setIsDelete(0);
+//		user.setRegistTime(new Date());
+//
+//		userService.saveUserTransactional(user);
+//
+//		return DxsJSONResult.ok("保存成功");
+//	}
+}
