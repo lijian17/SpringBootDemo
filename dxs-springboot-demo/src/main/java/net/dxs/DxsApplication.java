2e0bc78e85f469a26904be00971de70e9e2afd42
深情小建 <374452668@qq.com>
2018-08-26 13:02:23 +0800

    11.1、SpringBoot整合定时任务task 1、使用注解@EnableScheduling开启定时任务，会自动扫描 2、定义@Component作为组件被容器扫描 3、支持cron表达式 在线cron表达式生成器地址：http://cron.qqe2.com/

--- dxs-springboot-demo/src/main/java/net/dxs/DxsApplication.java
+++ dxs-springboot-demo/src/main/java/net/dxs/DxsApplication.java
@@ -3,6 +3,7 @@ package net.dxs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import tk.mybatis.spring.annotation.MapperScan;
 
@@ -11,6 +12,8 @@ import tk.mybatis.spring.annotation.MapperScan;
 @MapperScan(basePackages = "net.dxs.mapper")
 // 扫描 所有需要的包，包含一些自用的工具类包 所在的路径
 @ComponentScan(basePackages = { "net.dxs", "org.n3r.idworker" })
+//开启定时任务
+@EnableScheduling
 public class DxsApplication {
 
 	public static void main(String[] args) {
