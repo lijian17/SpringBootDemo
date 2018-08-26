e03d550d1f6732161fc6898e63c56f01c5de8416
深情小建 <374452668@qq.com>
2018-08-26 12:30:55 +0800

    10.2、SpringBoot使用已封装好的Redis工具类进行操作 1、注入工具类 2、工具类的使用示例 3、附上工具类RedisOperator.java

--- dxs-springboot-demo/src/main/java/net/dxs/controller/RedisController.java
+++ dxs-springboot-demo/src/main/java/net/dxs/controller/RedisController.java
@@ -1,6 +1,8 @@
 package net.dxs.controller;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -9,7 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import net.dxs.pojo.DxsJSONResult;
 import net.dxs.pojo.SysUser;
+import net.dxs.pojo.User;
 import net.dxs.utils.JsonUtils;
+import net.dxs.utils.RedisOperator;
 
 @RestController
 @RequestMapping("redis")
@@ -17,6 +21,9 @@ public class RedisController {
 
 	@Autowired
 	private StringRedisTemplate strRedis;
+	
+	@Autowired
+	private RedisOperator redis;
 
 	@RequestMapping("/test")
 	public DxsJSONResult test() {
@@ -30,5 +37,36 @@ public class RedisController {
 		strRedis.opsForValue().set("json:user", JsonUtils.objectToJson(user));
 		SysUser jsonUser = JsonUtils.jsonToPojo(strRedis.opsForValue().get("json:user"), SysUser.class);
 		return DxsJSONResult.ok(jsonUser);
+	}
+
+	@RequestMapping("/getJsonList")
+	public DxsJSONResult getJsonList() {
+		User user = new User();
+		user.setAge(18);
+		user.setName("54大学生");
+		user.setPassword("123456");
+		user.setBirthday(new Date());
+
+		User u1 = new User();
+		u1.setAge(19);
+		u1.setName("dxs");
+		u1.setPassword("123456");
+		u1.setBirthday(new Date());
+
+		User u2 = new User();
+		u2.setAge(17);
+		u2.setName("hello 54dxs");
+		u2.setPassword("123456");
+		u2.setBirthday(new Date());
+
+		List<User> userList = new ArrayList<>();
+		userList.add(user);
+		userList.add(u1);
+		userList.add(u2);
+
+		redis.set("json:info:userlist", JsonUtils.objectToJson(userList), 2000);
+		String userListJson = redis.get("json:info:userlist");
+		List<User> userListBorn = JsonUtils.jsonToList(userListJson, User.class);
+		return DxsJSONResult.ok(userListBorn);
 	}
 }
