8547851ea1d3ac2aca8e6eff6d56b6f2fafe5d45
深情小建 <374452668@qq.com>
2018-09-16 18:52:34 +0800

    发送html邮件

--- dxs-springboot-demo/src/main/java/net/dxs/service/MailService.java
+++ dxs-springboot-demo/src/main/java/net/dxs/service/MailService.java
@@ -1,9 +1,13 @@
 package net.dxs.service;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -32,6 +36,26 @@ public class MailService {
 		message.setSubject(subject);
 		message.setText(content);
 		message.setFrom(from);
+
+		mailSender.send(message);
+	}
+
+	/**
+	 * 发送HTML邮件
+	 * 
+	 * @param to      发送给
+	 * @param subject 发送主题
+	 * @param content 发送正文
+	 * @throws MessagingException
+	 */
+	public void sendHtmlMail(String to, String subject, String content) throws MessagingException {
+		MimeMessage message = mailSender.createMimeMessage();
+
+		MimeMessageHelper helper = new MimeMessageHelper(message, true);
+		helper.setTo(to);
+		helper.setSubject(subject);
+		helper.setText(content, true);
+		helper.setFrom(from);
 
 		mailSender.send(message);
 	}
--- dxs-springboot-demo/src/test/java/net/dxs/service/ServiceTest.java
+++ dxs-springboot-demo/src/test/java/net/dxs/service/ServiceTest.java
@@ -1,6 +1,7 @@
 package net.dxs.service;
 
 import javax.annotation.Resource;
+import javax.mail.MessagingException;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,14 +14,20 @@ public class ServiceTest {
 
 	@Resource
 	MailService mailService;
-	
+
 	@Test
 	public void sayHelloTest() {
 		mailService.sayHello();
 	}
-	
+
 	@Test
 	public void sendSimpleMailTest() {
 		mailService.sendSimpleMail("lijian_17@163.com", "这是第一封邮件", "搭建好，这是我的第一封邮件");
+	}
+
+	@Test
+	public void sendHtmlMailTest() throws MessagingException {
+		String content = "<html><body><h3>hello world，这是一封html邮件<h3></body></html>";
+		mailService.sendHtmlMail("lijian_17@163.com", "这是一封html邮件", content);
 	}
 }
